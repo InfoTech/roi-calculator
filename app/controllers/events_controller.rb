cedffad6da1a13d044241a2b66a1a454c74a5908
Ryan Zuwala <rzuwala@infotech.com>
2017-06-01 15:21:06 -0400

    very basic event listing. no styles or anything

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 require 'viewpoint'
+require 'time'
 include Viewpoint::EWS
 
 class EventsController < ApplicationController
@@ -9,44 +10,51 @@ class EventsController < ApplicationController
 
   def index
     calendar = get_calendar
-    @folders = get_folders
-
 
     @events = []
 
     calendar.items.each do |item|
-      #p item.subject
-      #p item.required_attendees
-
-      @events << item
+      if !item.cancelled?
+        @events << item
+      end
     end
   end
 
-  def create
-
-  end
-
   private
-  def get_calendar
+  def get_client
     # https://makandracards.com/makandra/27443-interacting-with-a-microsoft-exchange-server-from-ruby
     endpoint = 'https://webmail.infotech.com/ews/Exchange.asmx'
     user = Rails.application.secrets.exchange_username
     pass = Rails.application.secrets.exchange_password
 
     client = Viewpoint::EWSClient.new(endpoint, user, pass, http_opts: {ssl_verify_mode: 0})
+  end
 
-    # calendar = client.get_folder(:calendar, opts = {act_as: "tomtg-hall@infotech.com"})
+  def get_calendar
+    client = get_client
+
+    #calendar = client.get_folder(:calendar, opts = {act_as: "tomtg-hall@infotech.com"})
     calendar = client.get_folder(:calendar)
   end
 
-  def get_folders
-    endpoint = 'https://webmail.infotech.com/ews/Exchange.asmx'
-    user = Rails.application.secrets.exchange_username
-    pass = Rails.application.secrets.exchange_password
+  # def get_availability
+  #   client = get_client
+  #
+  #   # Find when a user is busy
+  #   start_time = DateTime.parse("2017-05-01").iso8601
+  #   end_time = DateTime.parse("2017-05-12").iso8601
+  #   user_free_busy = client.get_user_availability(['tomtg-hall@infotech.com'],
+  #     start_time: start_time,
+  #     end_time:   end_time,
+  #     requested_view: :free_busy)
+  #   busy_times = user_free_busy.calendar_event_array
+  # end
 
-    client = Viewpoint::EWSClient.new(endpoint, user, pass, http_opts: {ssl_verify_mode: 0})
 
-    client.folders
-  end
+  # def get_folders
+  #   client = get_client
+  #
+  #   client.folders
+  # end
 
 end
