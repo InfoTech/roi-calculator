2df3c06e6b519cc9826751a1cf4c6e75608f2259
Sergiy Kuzmenko <s.kuzmenko@gmail.com>
2017-06-02 09:29:41 -0400

    Listing events by month

--- app/services/exchange_client_service.rb
+++ app/services/exchange_client_service.rb
@@ -1,10 +1,30 @@
 class ExchangeClientService
+  ROOMS = [
+    { slug: :concert_hall, name: 'Fake Concert Hall Calendar' },
+    { slug: :red_room, name: 'Fake Red Room Calendar' }
+  ]
   class << self
     def client
       endpoint = 'https://webmail.infotech.com/ews/Exchange.asmx'
       user = Rails.application.secrets.exchange_username
       pass = Rails.application.secrets.exchange_password
       Viewpoint::EWSClient.new(endpoint, user, pass, http_opts: {ssl_verify_mode: 0})
+    end
+
+    def events(slug: nil, name: nil, year: nil, month: nil)
+      raise 'Either slug or name is required' unless slug || name
+
+      calendar = slug ? client.get_folder(slug) : client.get_folder_by_name(name)
+      if year && month
+        items = calendar.items_between(*DateLib.date_range(year: year, month: month))
+      else
+        items = calendar.items
+      end
+      items.reject(&:cancelled?)
+    end
+
+    def find_room(slug)
+      ROOMS.find { |r| r[:slug] == slug }
     end
   end
 end
